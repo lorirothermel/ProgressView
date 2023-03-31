3fe9a37854a17ebf6cb695d66d77e878c7cf5cb9
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-03-31 14:21:41 -0400

    Initial Commit

---
+++ ProgressView/ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewApp.swift
+//  ProgressView
+//
+//  Created by Lori Rothermel on 3/31/23.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
